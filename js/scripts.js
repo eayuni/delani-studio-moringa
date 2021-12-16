e2b0cd5fa5a9d87f1a884a23d8b00adb94f659ce
Eunice Otieno <otieunice@gmail.com>
2021-12-16 11:25:25 +0300

    Added hover animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,36 @@
-$(document).ready(function () {
-    $(".image1").click(function () {
-        $(".image1").hide(1200);
-        $(".p1").toggle(1200);
-        $(".p1").show(1200);
+$(document).ready(function(){
+    $(".image1").click(function(){
+      $(".image1").hide(1200);
+      $(".p1").toggle(1200);
+      $(".p1").show(1200);
     })
-    $(".p1").click(function () {
-        $(".p1").hide(1200);
-        $(".image1").toggle(1200);
-        $(".image1").show(1200);
+    $(".p1").click(function(){
+      $(".p1").hide(1200);
+      $(".image1").toggle(1200);
+      $(".image1").show(1200);
     });
-});
-
-$(document).ready(function () {
-    $(".image2").click(function () {
+  });
+  $(document).ready(function(){
+      $(".image2").click(function(){
         $(".image2").hide(1200);
         $(".p2").toggle(1200);
-        $(".p2").toggle(1200);
-    })
-    $(".p2").click(function () {
+        $(".p2").show(1200);
+      })
+      $(".p2").click(function(){
         $(".p2").hide(1200);
         $(".image2").toggle(1200);
         $(".image2").show(1200);
-    })
-});
-
-$(document).ready(function () {
-    $(".image3").click(function () {
-        $(".image3").hide(1200);
-        $(".p3").toggle(1200);
-        $(".p3").toggle(1200);
-    })
-    $(".p3").click(function () {
-        $(".p3").hide(1200);
-        $(".image3").toggle(1200);
-        $(".image3").show(1200);
-    })
-});
+      });
+  });
+  $(document).ready(function(){
+      $(".image3").click(function(){
+          $(".image3").hide(1200);
+          $(".p3").toggle(1200);
+          $(".p3").show(1200);
+      })
+      $(".p3").click(function(){
+          $(".p3").hide(1200);
+          $(".image3").toggle(1200);
+          $(".image3").show(1200);
+      })
+  })
